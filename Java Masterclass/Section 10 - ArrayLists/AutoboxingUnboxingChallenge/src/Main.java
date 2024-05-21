e134e6c73f3829beaf3dc152ceade6855eb8bf8f
ahmedwashere <aeldeso@xula.edu>
2024-05-21 17:57:45 -0500

    Updated Code

---
+++ Java Masterclass/Section 10 - ArrayLists/AutoboxingUnboxingChallenge/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Ill need a few things
+        // Class for Customer
+        // ArrayList of Integers wrapper.
+        // Calculate total amount which uses unboxing.
+        // Should also print a way to print All transactions
+        // Add transaction to list
+        // Class for Bank
+        // Method to add customer to Bank
+        // Method to have a transaction for a customer.
+    }
+}
